658f40d8f848c2bf4b7f5e9806958eb14acb13b2
harikrishna <hchalla2020@gmail.com>
2019-09-21 21:30:38 +0530

    This is first commit

---
+++ SampleProject/src/test/java/Maven/SampleProject/Login.java
@@ -0,0 +1,14 @@
+package Maven.SampleProject;
+
+import org.testng.annotations.Test;
+
+public class Login {
+	
+@Test	
+public void Login123() {
+	
+	System.out.println("Hello Jenkins");
+	
+	}
+
+}
